b53540d19f853d31748a8d9a649949ad0f2584f7
Waleedr4 <w772866685@gmail.com>
2024-12-20 03:58:14 +0300

    lab1 2024.12.5

---
+++ index.php
@@ -0,0 +1,62 @@
+\\مثال if
+
+<?php
+ number = 10$;
+ if ($number > 5)
+ {
+
+    echo "the number is greater than 5";
+ }
+ ?>
+
+
+\\if & else مثال
+ <?php
+ number = 3$;
+ if($number % 2 == 0)
+ {
+    echo " the number is even"
+
+ }
+
+ else{
+
+    each "the number is odd"
+ }
+?>
+
+
+\\ switchمثال 
+
+<?php
+
+day ="$"
+
+    {
+        switch ($day)
+
+    
+        case1: "monday";
+        echo "start of is work week";break
+
+        case2: "friday";
+        echo "weekend is near";break
+        
+        case3: "sunday";
+        echo "it's a relaxing";break
+        
+        default:
+        echo "it's a regular day
+         
+    }
+    
+?>
+
+
+ do & while مثال 
+
+<?php
+ count =10$ ;
+ 
+ do         
+
